d149e9fece88ef15303f25efe7f4c0ef36f8e2eb
Kathryn DiPippo <dipippo.k@gmail.com>
2019-02-05 21:14:33 -0500

    Debug 1

--- jabberwock-isle.com/index_loggedin.php
+++ jabberwock-isle.com/index_loggedin.php
@@ -42,6 +42,7 @@ function printNestedRecipe($conn, $userid, $ingredientid, $diff) {
 
 <h1 class="text-center">Hello, <?php echo $_SESSION['user']->getUsername()?>!</h1>
 <?php
+print_r($conn);
 $primaryStudentSql = mysqli_query($conn, "SELECT studentname FROM siteusersettings LEFT JOIN students ON students.studentid=siteusersettings.mainstudentid WHERE userid=".$_SESSION['user']->getID().";");
 $primaryStudentRow = mysqli_fetch_assoc($primaryStudentSql);
 ?>
